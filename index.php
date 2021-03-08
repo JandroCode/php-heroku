bf03fcd4f4c1207dba78156dfa0c4fb89d3664d3
JandroCode <Algarrobeando@hotmail.com>
2021-03-08 19:32:07 +0100

    Añadiendo estilos

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *{
+            font-family:sans-serif;
+        }
+    </style>
+
+</head>
+<body>
+
 <?php
+    echo '<h1>Hola Heroku</h1>';
+    $con = mysqli_connect('bqj2gugwnfj3zkf5agyj-mysql.services.clever-cloud.com','uj1rpm7iqaaivv7f','iykF3R0e8ksfyjPEEHjy');
+    echo '<br>';
+    
+    if($con){
+        echo '<h2>Conectado a la base de datos de Clever Cloud</h2>';
+    }
+?>
+</body>
+</html>
 
 
-echo 'Hola Heroku';
 
 
-$con = mysqli_connect('bqj2gugwnfj3zkf5agyj-mysql.services.clever-cloud.com','uj1rpm7iqaaivv7f','iykF3R0e8ksfyjPEEHjy');
-
-if($con){
-    echo 'Conectado a la base de datos de Clever Cloud';
-}
