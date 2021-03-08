fa267bcd39267907badc9cb254fa6903fe59c3ac
JandroCode <Algarrobeando@hotmail.com>
2021-03-08 19:14:36 +0100

    Segundo commit

--- index.php
+++ index.php
@@ -2,3 +2,10 @@
 
 
 echo 'Hola Heroku';
+
+
+$con = mysqli_connect('bqj2gugwnfj3zkf5agyj-mysql.services.clever-cloud.com','uj1rpm7iqaaivv7f','iykF3R0e8ksfyjPEEHjy');
+
+if($con){
+    echo 'Conectado a la base de datos de Clever Cloud';
+}
